18257f95c1393e3c98f12661882fc2ec93c86f35
ImRandomguy <flyingsquirrelsarecool@gmail.com>
2014-08-24 17:14:41 -0500

    Update file.js

--- CLIENT_KEYS/file.js
+++ CLIENT_KEYS/file.js
@@ -1,2 +1,2 @@
-var clientId = 76e0353dbfd399a;
-var clientSecret = 944ed6ee0e627ecc1c2e8c0e38de13cdb32be720;
+var clientId = "76e0353dbfd399a";
+var clientSecret = "944ed6ee0e627ecc1c2e8c0e38de13cdb32be720";
