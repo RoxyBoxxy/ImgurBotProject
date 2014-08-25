1c88def911d84dd126531166bf776d336a3b8855
ImRandomguy <flyingsquirrelsarecool@gmail.com>
2014-08-24 19:40:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -12,16 +12,18 @@ function makeid()
 error = false;
 
 function shifter(){
-	check = newstr.charAt(0);
+	check = oldstr.charAt(0);
 	if (lengthcheck <= 50){
 	if (check !== " "){
-	newstr = newstr + check
+	subreddit = subreddit + check
 	lengthcheck = lengthcheck + 1;
 	shifter();
-	}}else{
+	} else {
+		stopAJAXtime();
+	} else{
 		error = true;
 		errortype = "Numerical";
-		errormessage = 'Your "subreddit" has a length of over 50 characters. Please buy a spacebar at http://www.laptopkey.com/';
+		errormessage = 'Your "subreddit" has a length of over 50 characters.';
 		prepareResponse();
 	}
 }
@@ -39,7 +41,7 @@ str2 = $('#messages').children()[$('#messages').children().length - 2].innerHTML
 str3 = $('#messages').children()[$('#messages').children().length - 3].innerHTML.toLowerCase();
  
         a = str.indexOf("imgurbot");
-	b = str.indexOf("image /r/");
+	b = str.indexOf("image /r/ ");
 	c = str.indexOf("random");
 	
 	if (a > -1){
@@ -47,11 +49,10 @@ str3 = $('#messages').children()[$('#messages').children().length - 3].innerHTML
 	if (b > -1){
 	var n = str.search(preimageregex);
 	var lengthcheck = 0;
-	var newstr = "";
+	var subreddit = "";
 	var oldstr = str.replace(imageregex, "");
 	if (n > -1){
 	shifter();
-	
 	} else {
 	error = true;
 	errortype = "Syntax";
@@ -65,3 +66,22 @@ str3 = $('#messages').children()[$('#messages').children().length - 3].innerHTML
 
     });
 });
+
+function stopAJAXtime(){
+$.ajax({
+      url: 'https://api.imgur.com/3/image',
+      method: 'POST',
+      headers: {
+        Authorization: authorization,
+        Accept: 'application/json'
+      },
+      data: {
+        image: localStorage.dataBase64,
+        type: 'base64'
+      },
+      success: function(result) {
+        var id = result.data.id;
+        window.location = 'https://imgur.com/gallery/' + id;
+      }
+    });
+}
