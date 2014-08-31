ac59459fbab1755a748aae7b53ecdc66bd71e045
ImRandomguy <flyingsquirrelsarecool@gmail.com>
2014-08-30 19:08:46 -0500

    Update meta.js

--- meta.js
+++ meta.js
@@ -1,1 +1,3 @@
-javascript:$('head').append(<script src="https://rawgit.com/ImRandomguy/ImgurBotProject/master/main.js"></script>);
+javascript:
+$('head').append('<script src="https://rawgit.com/ImRandomguy/ImgurBotProject/master/CLIENT_KEYS/file.js"></script>');
+$('head').append('<script src="https://rawgit.com/ImRandomguy/ImgurBotProject/master/main.js"></script>');
