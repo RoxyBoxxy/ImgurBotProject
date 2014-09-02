579bd46ce4461177eb1cc45dbd3580afebcd048c
ImRandomguy <flyingsquirrelsarecool@gmail.com>
2014-09-02 12:02:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -192,11 +192,14 @@ var authorization = 'Client-ID ' + clientId;
 \*/
 
 $('head').append('<script src="https://www.google.com/jsapi"></script>');
-google.load('feeds', 1, {
+
+setTimeout(function(){google();}, 2000);
+
+function google (google.load('feeds', 1, {
         callback: function() {
             // do stuff, if you want - it doesn't matter, because the page isn't blank!
         }
-    } );
+    } ));
 var feedlimit = 10;
 
 function loadit(){ 
