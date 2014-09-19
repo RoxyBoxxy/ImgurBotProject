c08d4a3e27a6dc04e23f9ac67d48cba5b79757ee
ImRandomguy <flyingsquirrelsarecool@gmail.com>
2014-09-19 12:28:59 -0500

    Delete meta.js

--- meta.js
+++
@@ -1,3 +0,0 @@
-javascript:
-$('head').append('<script src="https://rawgit.com/ImRandomguy/ImgurBotProject/master/CLIENT_KEYS/file.js"></script>');
-$('head').append('<script src="https://rawgit.com/ImRandomguy/ImgurBotProject/master/main.js"></script>');
