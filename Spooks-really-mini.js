45afb449ddf6cd43df333c7cc364a2fbe9b72936
krynomore <squires1993@gmail.com>
2014-11-20 21:58:19 +0000

    Using a part of brunos code

---
+++ Spooks-really-mini.js
@@ -0,0 +1,15 @@
+if (text.contains("!motd")) {
+            getmotd()
+       });
+        }
+            
+    }
+        
+});
+
+function getCount() { 
+    $.ajax({
+        url : "http://krynomore.tk/motd.php",
+        type : 'GET',
+        success : function(data) { send("Today, the motd is" + data + " Set By KrYnoMoRe."); }
+    });
